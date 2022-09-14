204cb05d4d18559f4739e3d02f84e44dbd9656b4
rani <rani@TEC-T-PC12>
2022-09-14 12:14:05 +0200

    Finished actors section

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,13 +8,20 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+
         [Display(Name = "Actor")]
+        [Required(ErrorMessage = "Picture is required")]
         public string? ProfilePictureURL { get; set; }
 
+
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is required")]
         public string? FullName { get; set; }
 
+
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "A description is required")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "Minimum 5 characters is required")]
         public string? Biography { get; set; }
 
 
